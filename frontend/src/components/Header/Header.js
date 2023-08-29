69cdb96f61f89f0d5056fd98cf030e0e358f49c6
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-08-29 15:37:06 +0530

    Added Landing Page

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">gistnotes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+            </Form>
+          </Nav>
+
+          <Nav>
+            <Nav.Link href="#">My Notes</Nav.Link>
+            <NavDropdown title="Krushna Kulkarni" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
