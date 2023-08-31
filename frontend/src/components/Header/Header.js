7b523fa1b0eef5bb66a8351e2f5efe29ad5f20a5
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-08-31 16:21:17 +0530

    added basic logout

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,9 +7,11 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
@@ -34,7 +36,14 @@ const Header = () => {
               <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item>Logout</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  localStorage.removeItem("userInfo");
+                  navigate("/");
+                }}
+              >
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
