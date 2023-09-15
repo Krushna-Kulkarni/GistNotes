cee4d934d9e0f738d0ca0a281c8cdcab42aebfa2
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-09-16 02:55:14 +0530

    made app production ready

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -12,12 +13,25 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
+
+// --------------Deployement---------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// --------------Deployement---------------
 
 app.use(notFound);
 app.use(errorHandler);
